ab00f4459719bbb14632d07e38e51a177a9b32a8
Johannes Hund <johannes.hund@siemens.com>
2016-12-14 16:20:25 +0100

    fixed naming convention and return types

--- src/tdparser.ts
+++ src/tdparser.ts
@@ -1,13 +1,13 @@
 import Thing from './thing'
 
-export function parseTdObj(td : Object) : Object {
+export function parseTDObj(td : Object) : Thing {
     return null;
 }
 
-export function parseTdString(td : string) : Object {
-    return null;
+export function parseTDString(td : string) : Thing {
+    return parseTDObj(JSON.parse(td));
 }
 
-export function serializeTd(thing : Thing) : Object {
+export function serializeTD(thing : Thing) : string {
     return null;
 }
