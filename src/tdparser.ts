4e3534de5ae148865992b261199ce5d47a5bd8fa
Sebastiankb <sebastian.kaebisch@siemens.com>
2017-01-11 16:49:27 +0100

    used typedJson modul for parsing TDs, add TD sample usage file

--- src/tdparser.ts
+++ src/tdparser.ts
@@ -2,18 +2,24 @@ import ThingDescription from './thingdescription'
 import ServedThing from './servedthing'
 import Servient from './servient'
 
+import { JsonMember, JsonObject, TypedJSON } from 'typedjson-npm';
+
 export function generateTD(thing : ServedThing, servient : Servient ) : ThingDescription {
+
     return null;
 }
 
-export function parseTDObj(td : Object) : ThingDescription {
-    return null;
+export function parseTDObj(td : ThingDescription) : string {
+
+        return TypedJSON.stringify(td);
 }
 
 export function parseTDString(td : string) : ThingDescription {
-    return parseTDObj(JSON.parse(td));
+        return TypedJSON.parse(td,ThingDescription);
 }
 
 export function serializeTD(thing : ThingDescription) : string {
+
+
     return null;
 }
--- src/thingdescription.ts
+++ src/thingdescription.ts
@@ -1,18 +1,18 @@
-import { JsonMember, JsonObject, TypedJSON } from 'typedjson-npm';
+import { JsonMember, JsonObject} from 'typedjson-npm';
 
 /** structured type representing a TD for internal usage */
 export default class ThingDescription {
 
-    @JsonMember({name : "@context"}) /** @context information of the TD */
+    @JsonMember({name : "@context",  elements: String }) /** @context information of the TD */
     private context : Array<string> = ["http://w3c.github.io/wot/w3c-wot-td-context.jsonld"]
 
-    @JsonMember({isRequired:true}) /** Human readable name identifier of the Thing  */
+    @JsonMember({isRequired:true, type: String}) /** Human readable name identifier of the Thing  */
     public name : string
 
-    @JsonMember /** base uri of the interaction resources */
+    @JsonMember({type: String}) /** base uri of the interaction resources */
     public base : string
 
-    @JsonMember({isRequired:true})  /** interactions of this thing */
+    @JsonMember({isRequired:true,  elements: Object })  /** interactions of this thing */
     public interactions : Array<TDInteraction>;
 
 }
@@ -22,26 +22,26 @@ export default class ThingDescription {
  */
 export class TDInteraction {
 
-    @JsonMember({name : "@type"}) /** @ type information of the interaction */
+    @JsonMember({name : "@type",  elements: String }) /** @ type information of the interaction */
     public  rdfType : Array<string>
 
-    @JsonMember({isRequired:true})  /** name/identifier of the interaction */
+    @JsonMember({isRequired:true, type: String})  /** name/identifier of the interaction */
     public name : string
 
     /** type of the interaction (action, property, event) */
     public interactionType : interactionTypeEnum
 
-    @JsonMember({isRequired:true})  /** link information of the interaction resources */
+    @JsonMember({isRequired:true,  elements: Object})  /** link information of the interaction resources */
     public links : Array<TDInteractionLink>;
 
-    @JsonMember /* writable flag for the property*/
+    @JsonMember({type: Boolean}) /* writable flag for the property*/
     public writable : boolean;
 
-    //how to handle types internally?
-    @JsonMember // json schema objects
+    //TODO: how to handle types internally?
+    @JsonMember({type: String}) // json schema objects
     public inputData : any
 
-    @JsonMember
+    @JsonMember({type: String})
     public outputDate : any
 }
 
@@ -49,13 +49,14 @@ export class TDInteraction {
 * Internal links information of an interaction
 */
 export class TDInteractionLink {
-  @JsonMember({isRequired:true})  /* relativ or absulut URI path of the interaction resource */
+  @JsonMember({isRequired:true, type: String})   /* relativ or absulut URI path of the interaction resource */
   public href : string
 
-  @JsonMember({isRequired:true})  /* used mediaType of the interacion resources */
+  @JsonMember({isRequired:true, type: String})   /* used mediaType of the interacion resources */
   public mediaType : mediaTypeEnum
 }
 
+/* media type selection */
 enum mediaTypeEnum {
     "application/json",
     "application/xml",
@@ -63,12 +64,13 @@ enum mediaTypeEnum {
     /* TODO: add more media types here */
 }
 
+/* interaction type selection */
 enum interactionTypeEnum {
     property,
     action,
     event
 }
-
+/* simple type selection */
 enum dataTypePrimitiveEnum {
     string,
     boolean,
