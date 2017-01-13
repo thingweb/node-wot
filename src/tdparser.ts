f4fd013bcd579d1a0502251cd9b45bdd3668b54f
Sebastiankb <sebastian.kaebisch@siemens.com>
2017-01-13 10:19:59 +0100

    normailze links if a base is defined

--- src/tdparser.ts
+++ src/tdparser.ts
@@ -13,7 +13,35 @@ export function parseTDObj(td : Object) : ThingDescription {
 }
 
 export function parseTDString(td : string) : ThingDescription {
-        return TypedJSON.parse(td,ThingDescription);
+
+        let td_obj = TypedJSON.parse(td,ThingDescription);
+        let base = td_obj.base
+
+        /* if a base uri is used normalize all relative hrefs in links */
+        if(base!=null) {
+
+          /** normalize each link information of the interaction */
+          for (let interaction of td_obj.interactions) {
+
+            let href = interaction.links[0].href
+
+            /* add '/' character if it is missing at the end of the base and
+            beginning of the href field*/
+            if(base.charAt(base.length-1)  != '/' &&  href.charAt(0) != '/') {
+              href = '/' + href;
+            }
+            /* remove '/' if it occurs at the end of base and
+            beginning of the href field */
+            else if(base.charAt(base.length-1)  == '/' &&  href.charAt(0) == '/') {
+              href = href.substr(1)
+            }
+            interaction.links[0].href = base + href;
+          }
+        }
+
+
+
+        return td_obj;
 }
 
 export function serializeTD(thing : ThingDescription) : string {
