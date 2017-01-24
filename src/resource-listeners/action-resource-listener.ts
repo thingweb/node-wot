6299d135c2cd8d721d206ebacedb6f033acf2bf1
Johannes Hund (h0ru5) <johannes.hund@gmail.com>
2017-01-24 21:01:12 +0100

    added license headers

--- src/resource-listeners/action-resource-listener.ts
+++ src/resource-listeners/action-resource-listener.ts
@@ -17,7 +17,6 @@
  * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-
 /**
  * Interaction resource that provides an Action
  */
