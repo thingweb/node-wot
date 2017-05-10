fbeb4d86473aca2b1c6f81cb3b1180f29a5ffdb0
Sebastiankb <sebastian.kaebisch@siemens.com>
2017-05-10 17:23:34 +0200

    add some complex type properties and actions to testthing.js

--- examples/testthing/testthing.js
+++ examples/testthing/testthing.js
@@ -22,7 +22,19 @@ WoT.createThing("testthing")
 
         thing
             .addProperty("array_property", { type: "array" })
-            .setProperty("array_property", []);
+            .setProperty("array_property", [2, ""]);
+
+        thing
+            .addProperty("complex_property", { "type": "object",
+                "properties": {
+                    "prop1": {"type": "integer"},
+                    "prop2": {"type": "string"}
+                },
+                "required": [
+                    "prop1",
+                    "prop2"
+                ]})
+            .setProperty("complex_property", {"prop1": 123, "prop2" : "abc"});
 
         thing
             .onUpdateProperty("num_property", (param) => {
@@ -32,14 +44,14 @@ WoT.createThing("testthing")
             );
 
         thing
-            .addAction("int_void_action")
+            .addAction("int_void_action", { type: "integer" })
             .onInvokeAction("int_void_action", function (param) {
                 let inputtype = typeof param;
                 console.log("int_void_action called with " + inputtype);
             });
 
         thing
-            .addAction("int_int_action")
+            .addAction("int_int_action",{ type: "integer" }, { type: "integer" })
             .onInvokeAction("int_int_action", function (param) {
                 let inputtype = typeof param;
                 console.log("int_int_action called with " + inputtype);
@@ -47,10 +59,41 @@ WoT.createThing("testthing")
             });
 
         thing
-            .addAction("string_void_action")
+            .addAction("string_void_action", { type: "string" })
             .onInvokeAction("string_void_action", function (param) {
                 let inputtype = typeof param;
                 console.log("string_void_action called with " + inputtype);
             });
+        thing
+            .addAction("void_complex_action", null, { "type": "object",
+                "properties": {
+                    "prop1": {"type": "integer"},
+                    "prop2": {"type": "string"}
+                },
+                "required": [
+                    "prop1",
+                    "prop2"
+                ]})
+            .onInvokeAction("void_complex_action", function (param) {
 
+                console.log("void_complex_action called  return " +  {"prop1": 123, "prop2" : "abc"});
+
+                return {"prop1": 123, "prop2" : "abc"};
+            });
+            thing
+            .addAction("complex_void_action", { "type": "object",
+                "properties": {
+                    "prop1": {"type": "integer"},
+                    "prop2": {"type": "string"}
+                },
+                "required": [
+                    "prop1",
+                    "prop2"
+                ]})
+            .onInvokeAction("complex_void_action", function (param) {
+                let inputtype = typeof param;
+                console.log("complex_void_action called  " + inputtype);
+
+                return {"prop1": 123, "prop2" : "abc"};
+            });
     });
