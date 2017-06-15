e8844475cf78a4d092d40eb3313328cf2521da4c
Johannes Hund (h0ru5) <johannes.hund@gmail.com>
2017-06-16 00:53:19 +0200

    also exposing http procol handlers

--- packages/node-wot/src/index.ts
+++ packages/node-wot/src/index.ts
@@ -42,3 +42,6 @@ export {default as ExposedThing} from './exposed-thing'
 export * from 'node-wot-td-tools'
 
 export {default as logger} from 'node-wot-logger'
+
+export {default as HttpServer} from 'node-wot-protocols-http-server'
+export {default as HttpClient} from 'node-wot-protocols-http-client'
