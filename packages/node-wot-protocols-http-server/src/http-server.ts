bcf725b6604ce724b1ba0bc4dd47f0c9bd0ceb69
Matthias Kovatsch <kovatsch@inf.ethz.ch>
2017-07-09 15:13:38 +0000

    Opened up CORS headers

--- packages/node-wot-protocols-http-server/src/http-server.ts
+++ packages/node-wot-protocols-http-server/src/http-server.ts
@@ -120,8 +120,8 @@ export default class HttpServer implements ProtocolServer {
     // Set CORS headers
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Request-Method', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
-    res.setHeader('Access-Control-Allow-Headers', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, HEAD, GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'content-type, *');
     if (req.method === 'OPTIONS') {
       res.writeHead(200);
       res.end();
