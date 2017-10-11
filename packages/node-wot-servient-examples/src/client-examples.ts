b1c184fa06d4c7e136fe10a6983f413bea2f35aa
Korkan Ege z003sbuf <ege.korkan.ext@siemens.com>
2017-10-11 14:42:24 +0200

    conflict resolve try5

--- packages/node-wot-servient-examples/src/client-examples.ts
+++ packages/node-wot-servient-examples/src/client-examples.ts
@@ -16,7 +16,6 @@
  * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
  * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-
 import ProtocolClient from "node-wot";
 import {HttpClientFactory} from "node-wot-protocol-http";
 import {CoapClientFactory} from "node-wot-protocol-coap";
