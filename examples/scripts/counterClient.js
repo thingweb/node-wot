83baebfcd370ccb53d9fc46690425566467f788d
danielpeintner <daniel@peintner.org>
2017-12-18 11:10:25 +0100

    align counter examples with new API

--- examples/scripts/counterClient.js
+++ examples/scripts/counterClient.js
@@ -1,9 +1,30 @@
 var targetUri = "http://localhost:8080/counter";
 var targetUriProperties = "http://localhost:8080/counter/properties";
 var counterUri = "http://localhost:8080/counter/properties/count";
-WoT.consume(targetUri).then(function(thing) {
-	thing.getProperty("count").then(function(count){
-		console.log("count value is ",count);
-    });
-	thing.invokeAction("increment");
-});
+WoT.fetchTD(targetUri)
+.then(function(td) {
+	let thing = WoT.consume(td);
+	
+	// read property
+	thing.readProperty("count")
+	.then(function(count){
+		console.log("count value is ", count);
+    })
+	.catch(err => { throw err });
+	
+	// increment property
+	thing.invokeAction("increment")
+	.then(function(count){
+		console.log("count value after increment is ", count);
+    })
+	.catch(err => { throw err });
+	
+	// decrement property
+	thing.invokeAction("decrement")
+	.then(function(count){
+		console.log("count value after decrement is ", count);
+    })
+	.catch(err => { throw err });
+	
+})
+.catch(err => { throw err });
