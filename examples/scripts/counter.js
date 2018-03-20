f589c4fca94e9a165c9e08ee842ec9172148f3c4
danielpeintner <daniel@peintner.org>
2018-03-20 11:35:09 +0100

    fix: counter example in JS

--- examples/scripts/counter.js
+++ examples/scripts/counter.js
@@ -5,50 +5,66 @@ const NAME_ACTION_DECREMENT = "decrement";
 const NAME_ACTION_RESET = "reset";
 
 let thing = WoT.produce({
-                name: "counter"
+    name: "counter"
 });
 
 console.log("Created thing " + thing.name);
 
 thing.addProperty({
-        name : NAME_PROPERTY_COUNT,
-        schema : '{ "type": "number"}',
-        value : 0,
-        observable : true,
-        writeable : true
+	name : NAME_PROPERTY_COUNT,
+	schema : '{ "type": "number"}',
+	value : 0,
+	observable : true,
+	writeable : true
+})
+
+thing.setPropertyWriteHandler( 
+	NAME_PROPERTY_COUNT,
+	(value) => {
+		console.log("Setting '" + NAME_PROPERTY_COUNT + "' value to " + value);
+	}
+);
+
+thing.addAction({
+    name : NAME_ACTION_INCREMENT
 })
 
 thing.addAction({
-        name : NAME_ACTION_INCREMENT
+    name : NAME_ACTION_DECREMENT
 })
 
 thing.addAction({
-        name : NAME_ACTION_DECREMENT
+    name : NAME_ACTION_RESET
 })
 
-thing.addAction({
-        name : NAME_ACTION_RESET
-})
+thing.setActionHandler( 
+	NAME_ACTION_RESET,
+	(parameters) => {
+		console.log("Resetting");
+		thing.writeProperty(NAME_PROPERTY_COUNT, 0);
+	}
+);
 
-thing.setActionHandler( () => {
-  console.log("Resetting");
-  thing.writeProperty(NAME_PROPERTY_COUNT, 0);
-  }, NAME_ACTION_RESET );
+thing.setActionHandler(
+	NAME_ACTION_INCREMENT,
+	(parameters) => {
+		console.log("Incrementing");
+		return thing.readProperty(NAME_PROPERTY_COUNT).then(function(count){
+			let value = count + 1;
+			thing.writeProperty(NAME_PROPERTY_COUNT, value);
+		});
+	}
+);
 
-thing.setActionHandler( () => {
-  console.log("Incrementing");
-  return thing.readProperty(NAME_PROPERTY_COUNT).then(function(count){
-    let value = count + 1;
-    thing.writeProperty(NAME_PROPERTY_COUNT, value);
-    })
-  }, NAME_ACTION_INCREMENT );
-
-thing.setActionHandler( () => {
-  console.log("Decrementing");
-  return thing.readProperty(NAME_PROPERTY_COUNT).then(function(count){
-    let value = count - 1;
-    thing.writeProperty(NAME_PROPERTY_COUNT, value);
-    })
-  }, NAME_ACTION_DECREMENT );
+thing.setActionHandler(
+	NAME_ACTION_DECREMENT,
+	(parameters) => {
+		console.log("Decrementing");
+		return thing.readProperty(NAME_PROPERTY_COUNT).then(function(count){
+			let value = count - 1;
+			thing.writeProperty(NAME_PROPERTY_COUNT, value);
+		});
+	}
+);
 
 thing.start();
